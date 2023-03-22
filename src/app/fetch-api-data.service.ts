5487d929c61437040f7ae28a53aa97a74c7ab31e
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-22 19:25:25 +0100

    imported necessary things and created a call to movie api

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from "rxjs";
+import { map } from "rxjs/operators";
+
+// Declaring the api url that will provide data for the client app
+const apiUrl = "https://movie-api.fly.dev/";
+
+// this decorator injects this service everywhere as a root
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FetchApiDataService {
+
+  // inject thr HttpClient module to the constructor params
+  // this will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) { }
+
+
