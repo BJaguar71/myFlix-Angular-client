97f25235e2c405dee4865d7979fbddbd42e1d8d7
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-23 16:34:00 +0100

    imported material modules to could create the registration and login form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,46 @@ import { AppComponent } from './app.component';
 // HttpClient
 import { HttpClientModule } from "@angular/common/http";
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material imports
+
+//  form input
+import { MatInputModule } from "@angular/material/input";
+// form button
+import { MatButtonModule } from "@angular/material/button";
+// form card
+import { MatCardModule } from "@angular/material/card";
+// form-field
+import { MatFormFieldModule } from "@angular/material/form-field";
+// dialog
+import { MatDialogModule } from "@angular/material/dialog";
+// snack bar
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+// form
+import { FormsModule } from "@angular/forms";
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationFormComponent,
+    UserLoginFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
