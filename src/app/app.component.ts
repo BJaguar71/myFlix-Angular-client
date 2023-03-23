6b3f985bf902b22a98e3c8a536945bb8fb8cef51
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-23 16:32:44 +0100

    added login and registration components to the app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from "@angular/material/dialog";
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  // pass MatDialog as an argument to be available to use in this component
+  constructor(public dialog: MatDialog) {}
+
+  // the function that will open the dialog when th signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+
+      // assigning the dialog a width
+      width: "300px"
+    });
+  }
+
+  // open login dialog
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: "300px"
+    })
+  }
 }
