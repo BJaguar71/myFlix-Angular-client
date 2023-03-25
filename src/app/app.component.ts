07216cb6ccf37306ce0b722334c37215043ce2b8
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-25 10:57:25 +0100

    updated the app component. welcome page is now the main view of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from "@angular/material/dialog";
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-
 
 @Component({
   selector: 'app-root',
@@ -13,30 +8,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  // pass MatDialog as an argument to be available to use in this component
-  constructor(public dialog: MatDialog) {}
-
-  // the function that will open the dialog when th signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-
-      // assigning the dialog a width
-      width: "300px"
-    });
-  }
-
-  // open login dialog
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: "300px"
-    })
-  }
-
-  // open movies dialog
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: "500px"
-    })
-  }
 }
