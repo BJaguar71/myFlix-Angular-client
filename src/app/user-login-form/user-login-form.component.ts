9237ca4a5174bb71e2a2dbd39fcb41105df55140
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-25 11:03:09 +0100

    updated the function that is responsible for logging in the user

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from "@angular/material/dialog";
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-user-login-form',
@@ -11,35 +12,40 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 
 export class UserLoginFormComponent implements OnInit {
 
-  @Input() loginData = { Username:" ", Password:" " };
+  @Input() loginData = { Username: " ", Password: " " };
 
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    private router: Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   loginUser(): void {
 
-    this.fetchApiData.userLogin(this.loginData).subscribe((response) => {
-
-      console.log("loginUser", response);
+    this.fetchApiData.userLogin(this.loginData).subscribe((result) => {
 
       // save the user login data in localStorage
-      localStorage.setItem("username", response.user.Username);
-      localStorage.setItem("token", response.token);
+      let user = result.user.Username;
+      let token = result.token;
 
+      localStorage.setItem("user", user);
+      localStorage.setItem("token", token);
+
+      console.log(user, token);
+
+      // logic for successful user login
       this.dialogRef.close();
-
-      this.snackBar.open("Login was successful!", "Ok", {
-        duration: 2000
+      //  once the user is successfully logged-in then change the route to the /movies
+      this.router.navigate(["movies"]);
+      this.snackBar.open("Login was successful!", "Ok",
+        { duration: 2000 });
+    },
+      (result) => {
+        this.snackBar.open(result, "Ok", {
+          duration: 2000
+        });
       });
-    }, (response) => {
-      console.log(response);
-      this.snackBar.open(response.message, "Ok", {
-        duration: 2000
-      });
-    });
   }
 }
