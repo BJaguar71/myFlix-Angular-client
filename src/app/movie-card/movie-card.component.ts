3f1e15b187d4d4ca370cfbcb2652e4469e1a67e7
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2023-03-25 11:00:20 +0100

    updated movie card comp
    added functions to show genre details
    
    added func to show director details
    
    added func to show movie summary

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { GenreComponent } from '../genre/genre.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DirectorComponent } from '../director/director.component';
+import { MovieSummaryComponent } from '../movie-summary/movie-summary.component';
 
 
 @Component({
@@ -13,7 +18,10 @@ export class MovieCardComponent {
   // define the movie variable and the of its value; which is here an array of the type any
   movies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) {}
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,) {}
 
   // call the function that fetches movies here. ngOnIniti(); is called when angular is done creating the component 
   ngOnInit(): void {
@@ -26,6 +34,39 @@ export class MovieCardComponent {
       this.movies = result;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openGenre(name: string, description: string): void {
+    this.dialog.open(GenreComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: "400px",
+      height: "auto",
+    });
+  }
+
+  openDirector(directorName: string, directorBio: string): void{
+    this.dialog.open(DirectorComponent, {
+      data: {
+        Name: directorName,
+        Bio: directorBio,
+      },
+      width: "400px",
+      height: "auto",
+    });
+  }
+
+  openSummary(title: string, summary: string): void {
+    this.dialog.open(MovieSummaryComponent, {
+      data: {
+        Title: title,
+        Summary: summary,
+      },
+      width: "400px",
+      height: "auto"
     });
   }
 }
